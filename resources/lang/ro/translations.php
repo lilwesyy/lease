17967611a3dd0e6df8ce787fe329ae5d822a1737
mirco.carp <mirco.carp@training4u.it>
2025-02-12 09:19:11 +0100

    ro translations

---
+++ resources/lang/ro/translations.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'aboutUs' => 'Despre <span style="color:{{ $color }};">Noi</span>',
+    'whoWeAre' => 'Cine suntem?',
+    'aboutText' => "Lorem Ipsum este un text demonstrativ utilizat în industria tipografică și de editare. Lorem Ipsum a fost standardul industriei încă din anii 1500, când un tipograf necunoscut a amestecat caracterele pentru a crea un specimen de tipar. A supraviețuit nu doar cinci secole, ci și tranziției către tehnoredactarea electronică, rămânând în esență neschimbat. A fost popularizat în anii 1960 odată cu lansarea foilor Letraset care conțineau pasaje din Lorem Ipsum și, mai recent, prin software-ul de publicare desktop precum Aldus PageMaker, care includea diverse versiuni ale Lorem Ipsum.",
+    'carRentalProcess' => 'Procesul de Închiriere Auto',
+    'chooseCar' => 'Alege o Mașină',
+    'chooseCarText' => 'Vizualizați gama noastră de mașini și găsiți mașina perfectă pentru zilele următoare.',
+    'comeInContact' => 'Contactați-ne',
+    'comeInContactText' => 'Echipa noastră de consultanți este pregătită să vă ajute cu procesul de rezervare sau cu orice întrebări.',
+    'enjoyDriving' => 'Bucurați-vă de Condus',
+    'enjoyDrivingText' => 'Primiți cheia și bucurați-vă de mașina dvs. Tratăm toate mașinile noastre cu respect.',
+    'belowCardText' => 'Dacă nu ați închiriat niciodată o mașină, vă vom ghida prin proces.',
+    'interestedInRenting' => 'Interesat de Închiriere?',
+    'sendMessage' => "Nu ezitați și trimiteți-ne un mesaj.",
+];
